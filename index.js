2a4ab124e5171ee3d57e352ea88ae28a488acc4f
cpan001 <github email address>
2017-10-09 17:48:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const app = "I don't do much."
+
+const products = [
+  { name: 'Head & Shoulders Shampoo', price: 4.99, discount: .6 },
+  { name: 'Twinkies', price: 7.99, discount: .45 },
+  { name: 'Oreos', price: 6.49, discount: .8 },
+  { name: 'Jasmine-scented bath pearls', price: 13.99, discount: .7 }
+];
+
+function reduce(collection, cb, initialValue) {
+  let result = initialValue;
+  collection.forEach((product, index) => {result = cb(result, product, index, collection)})
+  return result;
+}
